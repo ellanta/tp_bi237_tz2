0f9dad6b23863099780a4e739caffd642f56b0a7
ellanta <piperika10@gmail.com>
2024-05-11 00:45:17 +0300

    new benchmarks and graph

--- src/main/java/Benchmarks/MainBenchmarks.java
+++ src/main/java/Benchmarks/MainBenchmarks.java
@@ -1,5 +1,6 @@
 package Benchmarks;
 
+import org.example.Main;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.results.RunResult;
 import org.openjdk.jmh.runner.Runner;
@@ -7,14 +8,170 @@ import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openjdk.jmh.runner.options.TimeValue;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Random;
 
 public class MainBenchmarks {
     @Benchmark
-    public void mybenchmark(){
-        int a = 2;
-        int b = 3;
-        int c = a + b;
+    public void test_sum10(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        for(int i = 0; i < 10; i++) {
+            Random random = new Random();
+            long randomNumber = random.nextLong() % 21 - 10;
+            arr.add(randomNumber);
+        }
+        Main._sum(arr);
+    }
+    @Benchmark
+    public void test_sum100(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        for(int i = 0; i < 100; i++) {
+            Random random = new Random();
+            long randomNumber = random.nextLong() % 21 - 10;
+            arr.add(randomNumber);
+        }
+        Main._sum(arr);
+    }
+    @Benchmark
+    public void test_sum1000(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        for(int i = 0; i < 1000; i++) {
+            Random random = new Random();
+            long randomNumber = random.nextLong() % 21 - 10;
+            arr.add(randomNumber);
+        }
+        Main._sum(arr);
+    }
+    @Benchmark
+    public void test_sum10000(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        for(int i = 0; i < 10000; i++) {
+            Random random = new Random();
+            long randomNumber = random.nextLong() % 21 - 10;
+            arr.add(randomNumber);
+        }
+        Main._sum(arr);
+    }
+    @Benchmark
+    public void test_mult10(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        for(int i = 0; i < 10; i++) {
+            Random random = new Random();
+            long randomNumber = random.nextLong() % 21 - 10;
+            arr.add(randomNumber);
+        }
+        Main._mult(arr);
+    }
+    @Benchmark
+    public void test_mult100(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        for(int i = 0; i < 100; i++) {
+            Random random = new Random();
+            long randomNumber = random.nextLong() % 21 - 10;
+            arr.add(randomNumber);
+        }
+        Main._mult(arr);
+    }
+    @Benchmark
+    public void test_mult1000(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        for(int i = 0; i < 1000; i++) {
+            Random random = new Random();
+            long randomNumber = random.nextLong() % 21 - 10;
+            arr.add(randomNumber);
+        }
+        Main._mult(arr);
+    }
+    @Benchmark
+    public void test_mult10000(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        for(int i = 0; i < 10000; i++) {
+            Random random = new Random();
+            long randomNumber = random.nextLong() % 21 - 10;
+            arr.add(randomNumber);
+        }
+        Main._mult(arr);
+    }
+    @Benchmark
+    public void test_min10(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        for(int i = 0; i < 10; i++) {
+            Random random = new Random();
+            long randomNumber = random.nextLong() % 21 - 10;
+            arr.add(randomNumber);
+        }
+        Main._min(arr);
+    }
+    @Benchmark
+    public void test_min100(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        for(int i = 0; i < 100; i++) {
+            Random random = new Random();
+            long randomNumber = random.nextLong() % 21 - 10;
+            arr.add(randomNumber);
+        }
+        Main._min(arr);
+    }
+    @Benchmark
+    public void test_min1000(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        for(int i = 0; i < 1000; i++) {
+            Random random = new Random();
+            long randomNumber = random.nextLong() % 21 - 10;
+            arr.add(randomNumber);
+        }
+        Main._min(arr);
+    }
+    @Benchmark
+    public void test_min10000(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        for(int i = 0; i < 10000; i++) {
+            Random random = new Random();
+            long randomNumber = random.nextLong() % 21 - 10;
+            arr.add(randomNumber);
+        }
+        Main._min(arr);
+    }
+    @Benchmark
+    public void test_max10(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        for(int i = 0; i < 10; i++) {
+            Random random = new Random();
+            long randomNumber = random.nextLong() % 21 - 10;
+            arr.add(randomNumber);
+        }
+        Main._max(arr);
+    }
+    @Benchmark
+    public void test_max100(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        for(int i = 0; i < 100; i++) {
+            Random random = new Random();
+            long randomNumber = random.nextLong() % 21 - 10;
+            arr.add(randomNumber);
+        }
+        Main._max(arr);
+    }
+    @Benchmark
+    public void test_max1000(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        for(int i = 0; i < 1000; i++) {
+            Random random = new Random();
+            long randomNumber = random.nextLong() % 21 - 10;
+            arr.add(randomNumber);
+        }
+        Main._max(arr);
+    }
+    @Benchmark
+    public void test_max10000(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        for(int i = 0; i < 10000; i++) {
+            Random random = new Random();
+            long randomNumber = random.nextLong() % 21 - 10;
+            arr.add(randomNumber);
+        }
+        Main._max(arr);
     }
     public static void main(String[] args) {
         Options opt = new OptionsBuilder()
--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -6,12 +6,111 @@ import java.util.ArrayList;
 
 public class BasicTest {
     @Test
-    public void test1(){
+    public void test_sum_1(){
         ArrayList<Long> arr = new ArrayList<Long>();
         arr.add(1l);
         arr.add(2l);
         arr.add(3l);
         long result = Main._sum(arr);
         Assertions.assertEquals(6l, result);
+    }
+    @Test
+    public void test_sum_2(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        arr.add(4l);
+        arr.add(5l);
+        arr.add(6l);
+        long result = Main._sum(arr);
+        Assertions.assertEquals(15l, result);
+    }
+    @Test
+    public void test_sum_3(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        arr.add(1000l);
+        arr.add(2000l);
+        arr.add(4000l);
+        long result = Main._sum(arr);
+        Assertions.assertEquals(7000l, result);
+    }
+    @Test
+    public void test_mult_1(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        arr.add(1l);
+        arr.add(2l);
+        arr.add(3l);
+        long result = Main._mult(arr);
+        Assertions.assertEquals(6l, result);
+    }
+    @Test
+    public void test_mult_2(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        arr.add(4l);
+        arr.add(5l);
+        arr.add(6l);
+        long result = Main._mult(arr);
+        Assertions.assertEquals(120l, result);
+    }
+    @Test
+    public void test_mult_3(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        arr.add(100l);
+        arr.add(200l);
+        arr.add(400l);
+        long result = Main._mult(arr);
+        Assertions.assertEquals(8000000l, result);
+    }
+    @Test
+    public void test_min_1(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        arr.add(1l);
+        arr.add(2l);
+        arr.add(3l);
+        long result = Main._min(arr);
+        Assertions.assertEquals(1l, result);
+    }
+    @Test
+    public void test_min_2(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        arr.add(4l);
+        arr.add(5l);
+        arr.add(6l);
+        long result = Main._min(arr);
+        Assertions.assertEquals(4l, result);
+    }
+    @Test
+    public void test_min_3(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        arr.add(100l);
+        arr.add(200l);
+        arr.add(400l);
+        long result = Main._min(arr);
+        Assertions.assertEquals(100l, result);
+    }
+    @Test
+    public void test_max_1(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        arr.add(1l);
+        arr.add(2l);
+        arr.add(3l);
+        long result = Main._max(arr);
+        Assertions.assertEquals(3l, result);
+    }
+    @Test
+    public void test_max_2(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        arr.add(4l);
+        arr.add(5l);
+        arr.add(6l);
+        long result = Main._max(arr);
+        Assertions.assertEquals(6l, result);
+    }
+    @Test
+    public void test_max_3(){
+        ArrayList<Long> arr = new ArrayList<Long>();
+        arr.add(100l);
+        arr.add(200l);
+        arr.add(400l);
+        long result = Main._max(arr);
+        Assertions.assertEquals(400l, result);
     }
 }
